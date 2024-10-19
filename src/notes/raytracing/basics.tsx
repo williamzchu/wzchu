3fcf64ac38150bcc1f0f5b9311c772e9a12be311
williamzchu@gmail.com <williamzchu@gmail.com>
2024-10-20 00:55:51 +0900

    raytracing/basics update

--- src/notes/raytracing/basics.tsx
+++ src/notes/raytracing/basics.tsx
@@ -1,5 +1,8 @@
 import Code from "../../Code"
 import camera from "./pinhole-camera.png"
+import { InlineMath, BlockMath } from 'react-katex';
+import 'katex/dist/katex.min.css';
+import { MathJax, MathJaxContext } from "better-react-mathjax";
 
 const sendrays = `
 for (int j = 0; j < image_height; j++) {
@@ -15,6 +18,7 @@ for (int j = 0; j < image_height; j++) {
 export default function RaytracingBasics(){
     return (
 <div>
+<MathJaxContext>
 <p>Perhaps before we start talking about tracing anything, we need to be able to see. We ultimately need that camera. 
 This leads into one of the reasons why this concept of ray-tracing is so beautiful, because of it's start with the camera.
 </p>
@@ -39,9 +43,22 @@ As a result, we can simply just cast rays from a focal point towards our models,
 <p>Basic raytracing ultimately relies on this bit of code, or a variation of it. </p>
 <p>Now another thing that has to be mentioned is that the above code only takes one ray per pixel. 
 However, for the classical brute force ray-tracers, also known as Monte-Carlo ray-tracers, we shoot multiple rays for each pixel, with random variation.
-This is known as sampling, and allows us to approximate the continuous integral in the light transport equation or the rendering equation!
-
 </p>
 
+<p> 
+    We first need to take a look at what equation we are modeling with these rays. Although the rendering equation is actually quite complex with many parameters, 
+    here is one <a href="https://en.wikipedia.org/wiki/Rendering_equation">variation</a> that represents the reflected light <MathJax style={{display:"inline"}}>{"\\(    L_r   \\)"}</MathJax> along the vector <MathJax style={{display:"inline"}}>{"\\(    \\omega_0   \\)"}</MathJax> which is towards the viewer.
+</p>
+
+<MathJax style={{display:"inline"}}>{"\\(    L_r(\\text{x},\\omega_0, \\lambda, t) = \\int_\\omega f_r (\\text{x}, \\omega_i, \\omega_0, \\lambda, t) L_i(\\text{x}, \\omega_i, \\lambda, t) (\\omega_i \\cdot \\text{n}) \\text{d} \\omega_i \\)"}</MathJax>
+
+<p>We can see that this represents a continuous integral over <MathJax style={{display:"inline"}}>{"\\( \\omega  \\)"}</MathJax> which represents a hemisphere of the point we are observing.</p>
+
+<p>Now obviously, we don't take a continuous integral in computers, adding an infinite number of small sections.
+Instead we sample, resulting in why we show rays with random variation, allowing us to approximate this integral.
+More samples would give better approximations as it is able to represent more rays in our integrals, yet comes with computational cost.    
+</p> 
+
+</MathJaxContext>
 </div>)
 }
